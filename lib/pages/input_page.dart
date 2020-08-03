434bdd03c72597f38acddfe6621b116b54524d11
Kaiser Nalle <japhet.moraleja@gmail.com>
2020-08-03 23:09:34 +0800

    Basic Layout

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -12,8 +12,77 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Color(0xff00274a),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 200,
+                  width: 200,
+                  child: Text('top 1'),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Color(0xff00274a),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 200,
+                  width: 200,
+                  child: Text('top 1-2'),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xff00274a),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              height: 200,
+              width: 400,
+              child: Text('middle 2'),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Color(0xff00274a),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 200,
+                  width: 200,
+                  child: Text('bottom 3'),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Color(0xff00274a),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 200,
+                  width: 200,
+                  child: Text('bottom 4'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
