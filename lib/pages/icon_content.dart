806c98c9fb5a369d288e7b018e1b02b126514229
Kaiser Nalle <japhet.moraleja@gmail.com>
2020-08-06 13:14:58 +0800

    structured icons and cards onto separate dart files

---
+++ lib/pages/icon_content.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class IconContent extends StatelessWidget {
+  IconContent({this.icon, this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.white,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
